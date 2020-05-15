45151e5f90ecbb7753da7254aa264c182eaed703
JZKnight <1328265557@qq.com>
2020-05-16 01:55:16 +0800

    commit netease app

---
+++ precache-manifest.5c487d3c80a7c6631fe9aab9fb7f227f.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c0b02224d9434a91b9",
+    "url": "css/app.343a80c5.css"
+  },
+  {
+    "revision": "3aaaf8a09eae38f0c798",
+    "url": "css/chunk-13a5154a.45807b09.css"
+  },
+  {
+    "revision": "ead353fd1c23473fb944",
+    "url": "css/chunk-3e757f88.69e8a310.css"
+  },
+  {
+    "revision": "66de9e1ae646cf10d7a0",
+    "url": "css/chunk-490ef009.56386a35.css"
+  },
+  {
+    "revision": "fc60e61cf238021099f6",
+    "url": "css/chunk-51175d2e.2dd37224.css"
+  },
+  {
+    "revision": "9c29b31f1668c74210d0",
+    "url": "css/chunk-71e3b089.98b3cee4.css"
+  },
+  {
+    "revision": "31103ad158e19b978f7e730ff5ac959b",
+    "url": "icon/demo.css"
+  },
+  {
+    "revision": "8c97e8471e6eacc797c999a182615188",
+    "url": "icon/demo_index.html"
+  },
+  {
+    "revision": "e81e728fa3a5cfd36aa4c8367a3a7600",
+    "url": "icon/iconfont.css"
+  },
+  {
+    "revision": "ac158f835c94afb5b2a70d6f8ce3f35c",
+    "url": "icon/iconfont.eot"
+  },
+  {
+    "revision": "8b403587d9eb326afe2b4f3202a67e70",
+    "url": "icon/iconfont.js"
+  },
+  {
+    "revision": "beab4ccb1bee365ec78b88452ae1bcc0",
+    "url": "icon/iconfont.json"
+  },
+  {
+    "revision": "2d1d9256f56d655e0a6d6a21a3eb3ace",
+    "url": "icon/iconfont.svg"
+  },
+  {
+    "revision": "73bf6598b535d3cfe69ef2e2028f8258",
+    "url": "icon/iconfont.ttf"
+  },
+  {
+    "revision": "9e011605a6e7cc9c8b24a69d12242a8e",
+    "url": "icon/iconfont.woff"
+  },
+  {
+    "revision": "2ec62f636dea3ab9e8611f5b7a306374",
+    "url": "icon/iconfont.woff2"
+  },
+  {
+    "revision": "d3bdd1080a72129346aa0b4b4964b75f",
+    "url": "image/disc.png"
+  },
+  {
+    "revision": "7c9b3adc16f5485c2bfbe8a540534188",
+    "url": "image/disc_default.png"
+  },
+  {
+    "revision": "8e1637243c6aa4c6e72ebe889be2ddf3",
+    "url": "image/download_logo_3x.png"
+  },
+  {
+    "revision": "2bfefd3ba37eaffbd66b7d108b9de26a",
+    "url": "image/hot_music_bg_3x.jpg"
+  },
+  {
+    "revision": "5207a28c3767992ca4bb6d4887c74880",
+    "url": "image/index_icon_2x.png"
+  },
+  {
+    "revision": "49a82fee3e0fc602e9c8b25df2bda159",
+    "url": "image/index_icon_3x.png"
+  },
+  {
+    "revision": "be4ebbeb6befadfcae75ce174e7db862",
+    "url": "image/needle-ip6.png"
+  },
+  {
+    "revision": "702cf6d95f29e2e594f53a3caab50e12",
+    "url": "image/needle.png"
+  },
+  {
+    "revision": "5c9abc4a022ec256b1d0b7dd12f4e0b0",
+    "url": "image/play.png"
+  },
+  {
+    "revision": "f3a9aaa781c3bf3b543d129327d35fce",
+    "url": "image/recommand_bg_3x.png"
+  },
+  {
+    "revision": "ec5e163ba5e2f25037839bedf3a7e075",
+    "url": "image/topbar.png"
+  },
+  {
+    "revision": "a6af3ec1d21986b9757d96e8601b8831",
+    "url": "image/up.png"
+  },
+  {
+    "revision": "59c96e845ab876a4c6ef0d79207b3c7a",
+    "url": "index.html"
+  },
+  {
+    "revision": "84c0b02224d9434a91b9",
+    "url": "js/app.b0081628.js"
+  },
+  {
+    "revision": "3aaaf8a09eae38f0c798",
+    "url": "js/chunk-13a5154a.45d21d77.js"
+  },
+  {
+    "revision": "e03cfec3f5ec9957737e",
+    "url": "js/chunk-2d0b1658.d17ac1e5.js"
+  },
+  {
+    "revision": "ead353fd1c23473fb944",
+    "url": "js/chunk-3e757f88.e585af31.js"
+  },
+  {
+    "revision": "922c6d1ea6e8daccef33",
+    "url": "js/chunk-47002422.4ce26710.js"
+  },
+  {
+    "revision": "66de9e1ae646cf10d7a0",
+    "url": "js/chunk-490ef009.23ab7520.js"
+  },
+  {
+    "revision": "fc60e61cf238021099f6",
+    "url": "js/chunk-51175d2e.133ded21.js"
+  },
+  {
+    "revision": "9c29b31f1668c74210d0",
+    "url": "js/chunk-71e3b089.d4f9f37a.js"
+  },
+  {
+    "revision": "eae71d20a42657a7ff57",
+    "url": "js/chunk-vendors.6c293e2c.js"
+  },
+  {
+    "revision": "cfe7f50de955e745ef3fe5f770432429",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "4561ed1dfd4e7588dbc7fe8c8ad5f389",
+    "url": "phone_common.css"
+  }
+]);
